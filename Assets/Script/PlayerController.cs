d855b0976440dfdde9933c3bbe841866aa1f7b60
P7ngu <60442992+P7ngu@users.noreply.github.com>
2023-12-28 09:22:52 +0100

    .

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //Want more control? Call methods on rigidbody and don't use Transform!
+    private Rigidbody playerRb;
+
+    public float jumpForce;
+    public float gravityModifier;
+    public bool isOnGround = true;
+
     // Start is called before the first frame update
     void Start()
     {
+        playerRb = GetComponent<Rigidbody>();
+
+        //playerRb.AddForce(Vector3.up * 1000);
+        //This makes the player jump, and then fall because of gravity ^
+
+        Physics.gravity *= gravityModifier;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
     }
 }
+
